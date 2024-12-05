f0086dbcb48a1051cdf4a75444ce5d03c6d78276
MuhammedKans <kanskabeer@gmail.com>
2024-12-05 07:45:22 +0530

    mongodb connection string changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose  = require("mongoose");
 
 const connectDB = async ()=>{
- await mongoose.connect("mongodb+srv://kanskabeer:muhammedkans0675@cluster0.gq5dq.mongodb.net/devchat")
+ await mongoose.connect(
+  "mongodb+srv://kanskabeer:muhammedkans0675@muhammed0.gq5dq.mongodb.net/devChat")
 }
 module.exports = connectDB
+
 
