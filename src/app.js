5f62fc65d14be440d103105c6b711ae130a53e6f
Muhammed Kans <kanskabeer@gmail.com>
2025-01-19 11:48:46 +0530

    small changes cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 
   
 app.use(cors({
-  origin:"https://mkans-devchat.vercel.app",credentials:true,
+  origin:"https://mkans-dev-chat-web.vercel.app",credentials:true,
 })); 
 
 
